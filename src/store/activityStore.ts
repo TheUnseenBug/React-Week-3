4275dff7f96c8225a1311127d3bc2746926b8c22
dennis granheimer <dennis.granheimer@live.se>
2025-01-26 23:30:46 +0100

    updated store

--- src/store/activityStore.ts
+++ src/store/activityStore.ts
@@ -1,36 +1,58 @@
 import { create } from "zustand";
+import { persist, PersistOptions } from "zustand/middleware";
 import { Todo, TodoStore } from "../types/todo";
 
-const useTodoStore = create<TodoStore>((set) => ({
-  todos: [],
-  
-  // Lägger till todo i listan
-  addTodo: (todo: Todo) =>
-    set((state) => ({
-      todos: [
-        ...state.todos,
-        {
-          id: todo.id,
-          city: todo.city,
-          activities: todo.activities,
-          date: todo.date,
-        },
-      ],
-    })),
+const customStorage: PersistOptions<TodoStore, TodoStore>['storage'] = {
+  getItem: async (name) => {
+    const item = localStorage.getItem(name);
+    return item ? JSON.parse(item) : null; // Parse the JSON string back to an object
+  },
+  setItem: async (name, value) => {
+    localStorage.setItem(name, JSON.stringify(value)); // Stringify the object before storing
+  },
+  removeItem: async (name) => {
+    localStorage.removeItem(name);
+  },
+};
 
-  // Tar bort todo från listan
-  removeTodo: (id: string) =>
-    set((state) => ({
-      todos: state.todos.filter((todo) => todo.id !== id),
-    })),
+const useTodoStore = create<TodoStore>()(
+  persist(
+    (set) => ({
+      todos: [],
+      
+      // Add todo to the list
+      addTodo: (todo: Todo) =>
+        set((state) => ({
+          todos: [
+            ...state.todos,
+            {
+              id: todo.id,
+              city: todo.city,
+              activities: todo.activities,
+              date: todo.date,
+            },
+          ],
+        })),
 
-  // Redigera todo i listan
-  editTodo: (todo:Todo) =>
-    set((state) => ({
-      todos: state.todos.map((t) =>
-        t.id === todo.id ? { ...t, ...todo } : t
-      ),
-    })),
-}));
+      // Remove todo from the list
+      removeTodo: (id: string) =>
+        set((state) => ({
+          todos: state.todos.filter((todo) => todo.id !== id),
+        })),
+
+      // Edit todo in the list
+      editTodo: (todo: Todo) =>
+        set((state) => ({
+          todos: state.todos.map((t) =>
+            t.id === todo.id ? { ...t, ...todo } : t
+          ),
+        })),
+    }),
+    {
+      name: "todo-storage", // Name of the storage key
+      storage: customStorage, // Use the custom storage
+    }
+  )
+);
 
 export default useTodoStore;
